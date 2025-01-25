16f56ff856f765eb3560742c6f5f4ad3f7e8a0fe
Anna Cinelli <91215956+annacinelli@users.noreply.github.com>
2025-01-25 10:20:56 +0100

    Minor changes

--- core/mutual_information_binning.py
+++ core/mutual_information_binning.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils/')))
-from decorators import time_it
+from utils.decorators import time_it
 
 
 @time_it
